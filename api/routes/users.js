f12987ec2dca1c38e3a09be9324038f9ea05291b
PhongDo <dovanphong96@gmail.com>
2022-04-24 22:27:08 +0700

    convert json before returning

--- api/routes/users.js
+++ api/routes/users.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('Text to test');
+  res.json({
+    result: 'Text to test'
+  });
 });
 
 module.exports = router;
