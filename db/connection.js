55d2d1198572034f35626071bd976e9d767a93c2
Phong <dovanphong96@gmail.com>
2022-05-16 13:55:55 +0700

    Issue add image to select bootstrap (#1)
    * first try
    
    * show thumbnail in adding category

--- db/connection.js
+++ db/connection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const { DB_NAME, DB_USER, DB_PASS, DB_HOST } = process.env;
 // Creating new Object of mongoose
 //mongodb+srv://admin:<password>@cluster0.znbih.mongodb.net/covid?retryWrites=true&w=majority
-const url = `mongodb://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}?authSource=admin`;
+const url = `mongodb://${DB_USER}:${DB_PASS}@${DB_HOST}:27017/${DB_NAME}?authSource=admin`;
 const connection = mongoose.createConnection(url);
 // postgres://db:5432
 
